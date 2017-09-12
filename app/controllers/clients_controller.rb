64a0f377b8fc144f64bde8f8f577a385ba258309
Enrique Turcott <enrique.turcott@cimav.edu.mx>
2017-09-12 16:32:11 -0600

    Finalizar clientes

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -42,7 +42,7 @@ class ClientsController < ApplicationController
   def update
     respond_to do |format|
       if @client.update(client_params)
-        format.html { redirect_to @client, notice: 'Client was successfully updated.' }
+        format.html { redirect_to clients_path, notice: 'Cliente actualizado correctamente.' }
         format.json { render :show, status: :ok, location: @client }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class ClientsController < ApplicationController
   def destroy
     @client.destroy
     respond_to do |format|
-      format.html { redirect_to clients_url, notice: 'Client was successfully destroyed.' }
+      format.html { redirect_to clients_path, notice: 'Client was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
