4f262c9b6f1a94cea7353d2fd54303a1b884345f
Jonathan Hernández <ion@cimav.edu.mx>
2017-10-31 17:01:16 -0700

    Pantalla inicial de presupuestos, falta las requisiciones

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -37,7 +37,7 @@ class ProjectsController < ApplicationController
 
     respond_to do |format|
       if @project.save
-        format.html { redirect_to @project, notice: 'Project was successfully created.' }
+        format.html { redirect_to @project, notice: 'Proyecto creado.' }
         format.json { render :show, status: :created, location: @project }
       else
         format.html { render :new }
@@ -51,7 +51,7 @@ class ProjectsController < ApplicationController
   def update
     respond_to do |format|
       if @project.update(project_params)
-        format.html { redirect_to @project, notice: 'Project was successfully updated.' }
+        format.html { redirect_to @project, notice: 'Proyecto actualizado.' }
         format.json { render :show, status: :ok, location: @project }
       else
         format.html { render :edit }
@@ -65,7 +65,7 @@ class ProjectsController < ApplicationController
   def destroy
     @project.destroy
     respond_to do |format|
-      format.html { redirect_to projects_url, notice: 'Project was successfully destroyed.' }
+      format.html { redirect_to projects_url, notice: 'Proyecto eliminado.' }
       format.json { head :no_content }
     end
   end
--- app/models/project.rb
+++ app/models/project.rb
@@ -35,25 +35,71 @@ class Project < ApplicationRecord
     0
   end
 
-  def budget
-    1200100
+  def budget_resume
+    resume = {}
+    resume['autorizado'] = 0
+    resume['disponible'] = 0
+    self.budget_details.each do |row|
+      resume['autorizado'] += row['total_autorizado']
+      resume['disponible'] += row['total_disponible']
+    end
+    return resume
   end
-
-  def budget_expended
-    budget / 3
-  end
-
-  def budget_obligated
-    0
-  end
-
-  def budget_available
-    (budget - budget_expended - budget_obligated)
-  end
-
+  
   def end_schedule
     schedules.where(schedule_type: Schedule::PROJECT_END).first 
   end
+
+
+  def budget_details
+    sql = "SELECT 
+        ct05_partida AS partida, 
+        pr20_desc AS concepto,
+        SUM(Autorizado) AS total_autorizado, 
+        SUM(Pagado) AS total_pagado,
+        SUM(Devengado) AS total_ejercido,
+        SUM(Col2) AS total_col2,
+        SUM(PorComprobar) AS total_por_comprobar, 
+        SUM(Comprometido) AS total_comprometido,
+        SUM(Autorizado) - SUM(Pagado) - SUM(Devengado) - SUM(Comprometido) AS total_disponible
+      FROM (
+        SELECT ct05_partida, 
+          IF(pr12_columna = 1, SUM(ct05_hab_det-ct05_deb_det),0) AS Autorizado, 
+          IF(pr12_columna = 4, SUM(ct05_deb_det-ct05_hab_det),0) AS Pagado, 
+          IF(pr12_columna = 3, SUM(ct05_deb_det-ct05_hab_det),0) AS Devengado, 
+          IF(pr12_columna = 2, SUM(ct05_deb_det-ct05_hab_det),0) AS Col2, 
+          IF(pr12_columna = 6, SUM(ct05_deb_det-ct05_hab_det),0) AS PorComprobar,
+          IF(pr12_columna = 5, SUM(ct05_deb_det-ct05_hab_det),0) AS Comprometido 
+        FROM 
+          netmultix.ct05_2017 
+          LEFT JOIN netmultix.pr12 ON TRIM(ct05_cta_det) LIKE CONCAT(TRIM(pr12_pre_ini),'%') 
+        WHERE 
+          ct05_proyecto LIKE '#{self.erp_number}%' 
+          AND pr12_columna IN (1,2,3,4,5,6) 
+        GROUP BY ct05_partida, pr12_columna 
+        ORDER BY ct05_partida
+      ) subtabla
+      LEFT JOIN netmultix.pr20 ON ct05_partida = pr20_partida
+      GROUP BY ct05_partida, pr20_desc"
+
+    connection = ActiveRecord::Base.connection
+    result = connection.exec_query(sql)
+
+    rows = []
+    
+    result.each do |row| 
+      if row['total_col2'] == row['total_disponible'] 
+        row['total_devengado'] = 0.0 
+      else
+        row['total_devengado'] = row['total_col2'].abs + row['total_disponible']
+        row['total_disponible'] = row['total_col2']
+      end
+      rows << row
+    end
+
+    return rows
+  end
+
 
   private
 
